5d673afbe8fa680c1914a88539135af4b2b6ae11
Coyls <76538870+Coyls@users.noreply.github.com>
2021-02-03 11:34:18 +0100

    Partie 3 - Choix des cases

--- index.php
+++ index.php
@@ -3,14 +3,39 @@
 require "functions.php";
 
 // Un tableau de 3x3 par defaut avec des valeurs de cellules a null.
-$game = null;//todo instanciate me.
+$game = [[null,null,null],[null,null,null],[null,null,null]];//todo instanciate me.
 
 play($game);
 
 
-/*
 function showCharacter($cell): string
 {
-    //return string.
+
+    if ($cell === 1) {
+        return "X";
+    }
+    if ($cell === 1) {
+        return "O";
+    }
+    if ($cell === null) {
+        return " ";
+    }
+    return " ";
 }
-*/
+
+function isEmpty($game, $resX, $resY): bool
+{
+    return $game[$resX][$resY] === null;
+}
+
+function hasAnyEmptyCell($game): bool
+{
+    foreach ($game as $column) {
+        foreach ($column as $case) {
+            return $column[$case] === null;
+        }
+    }
+
+    return false;
+}
+
